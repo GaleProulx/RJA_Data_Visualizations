2faa00afdf23439776a4f70098b3beefa1b2da70
Gale Proulx <galeproulx@pop-os.localdomain>
2020-08-05 19:59:22 -0400

    Two new visualizations added. - DE_use_of_force.py has two new fisualizations. - vermont_population_2019.csv data added from US Census Bureau.

--- Use_of_Force/DE_use_of_force.py
+++ Use_of_Force/DE_use_of_force.py
@@ -9,7 +9,9 @@ Created on Tue Jul 14 17:33:33 2020
 # IMPORT DEPENDENCIES & SET CONFIGURATION
 # ############################################################################
 import altair as alt
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 500)
@@ -29,19 +31,66 @@ def import_csv(filename: str, suppress_stats=False):
     return df
 
 
+def linegraph(x: pd.Series, y: pd.Series, title='Title', bar_color='#E81E1E',
+              x_rotation=0):
+    plt.bar(x, y, color=bar_color)
+    plt.title(title)
+    plt.xticks(rotation=x_rotation)
+    plt.show()
+
+
 
 # MAIN
 # ############################################################################
 def main() -> None:
     df = import_csv('BPD_UseOfForce_2012_2018.csv')
-    race = df.groupby('race', as_index=False).sum()
-    print(df.groupby('race', as_index=False).sum())
+    c19 = import_csv('vermont_population_2019.csv')
     
-    alt.Chart(df).mark_bar().encode(
-        x='race',
-        y='officers_involved',
-        tooltip=['officers_involved']
-    ).interactive()
+    # [VISUALIZATION] USE OF FORCE RACE STATS 
+    race_dis = pd.DataFrame(df.race.value_counts())
+    race_dis.reset_index(inplace=True)
+    race_dis.rename(columns={'index': 'race', 'race': 'total_reports'}, inplace=True)
+    race_dis['percentage'] = ((race_dis['total_reports'] / race_dis['total_reports'].sum()) * 100)
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.bar(race_dis['race'], race_dis['total_reports'], color='#E81E1E')
+    plt.title('Number of Use of Force Reports (from 2012-2018) by Race', fontsize=15, y=1.03)
+    plt.xticks(rotation=30, fontsize=13)
+    plt.yticks(fontsize=13)
+    plt.text(-1, -450, 'Source: Burlington Police Open Data Sets', fontsize=12)
+    
+    # Thank you Esmailian for providing a quick solution to bar annotations.
+    # Source: https://datascience.stackexchange.com/questions/48035/how-to-show-percentage-text-next-to-the-horizontal-bars-in-matplotlib
+    for p in ax.patches:
+        percentage = '{:.1f}%'.format(100 * p.get_height()/race_dis['total_reports'].sum())
+        x = p.get_x() + p.get_width() - 0.53
+        y = p.get_y() + p.get_height() + 10
+        ax.annotate(percentage, (x, y), fontsize=12)
+        
+    plt.show()
+    
+    # [VISUALIZATION] POPULATION RACE STATS
+    fig, ax = plt.subplots(figsize=(10, 4))
+    plt.bar(c19['Race'], c19['Percentage'], color='green')    
+    plt.title('Vermont 2019 Population Percentages by Race', fontsize=15, y=1.03)
+    plt.xticks(rotation=90, fontsize=13)
+    plt.yticks(fontsize=13)
+    plt.ylim(0, 105)
+    plt.text(-1, -115, 'Source: United States Census Bureau', fontsize=12)
+
+    # Thank you Esmailian for providing a quick solution to bar annotations.
+    # Source: https://datascience.stackexchange.com/questions/48035/how-to-show-percentage-text-next-to-the-horizontal-bars-in-matplotlib
+    for p in ax.patches:
+        percentage = '{:.1f}%'.format(100 * p.get_height()/100)
+        x = p.get_x() + p.get_width() - 0.53
+        y = p.get_y() + p.get_height() + 3
+        ax.annotate(percentage, (x, y), fontsize=12)
+        
+    plt.show()
+
+    # OTHER STUFF    
+    race = df.groupby('race', as_index=False).sum()
+    print(c19)
     
 
 if __name__ == "__main__":
