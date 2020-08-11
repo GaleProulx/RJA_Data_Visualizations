fc87afd6633a0881685bc1d961a7ee5984687375
Gale Proulx <galeproulx@pop-os.localdomain>
2020-08-11 18:30:13 -0400

    New iteration on visualizations for Use of Force. - Modified Use of Force statistics to be by year. - Added Burlington population census data for 2019.

--- Use_of_Force/DE_use_of_force.py
+++ Use_of_Force/DE_use_of_force.py
@@ -44,39 +44,44 @@ def linegraph(x: pd.Series, y: pd.Series, title='Title', bar_color='#E81E1E',
 # ############################################################################
 def main() -> None:
     df = import_csv('BPD_UseOfForce_2012_2018.csv')
-    c19 = import_csv('vermont_population_2019.csv')
+    df['date'] = df['date'].apply(lambda date: pd.to_datetime(date, format='%Y-%m-%d %H:%M:%S'))
+    df['year'] = df['date'].apply(lambda date: date.year)
+    c19 = import_csv('burlington_population_2019.csv')
+    years = list(df['year'].unique())
     
-    # [VISUALIZATION] USE OF FORCE RACE STATS 
-    race_dis = pd.DataFrame(df.race.value_counts())
-    race_dis.reset_index(inplace=True)
-    race_dis.rename(columns={'index': 'race', 'race': 'total_reports'}, inplace=True)
-    race_dis['percentage'] = ((race_dis['total_reports'] / race_dis['total_reports'].sum()) * 100)
-    
-    fig, ax = plt.subplots(figsize=(10, 6))
-    plt.bar(race_dis['race'], race_dis['total_reports'], color='#E81E1E')
-    plt.title('Number of Use of Force Reports (from 2012-2018) by Race', fontsize=15, y=1.03)
-    plt.xticks(rotation=30, fontsize=13)
-    plt.yticks(fontsize=13)
-    plt.text(-1, -450, 'Source: Burlington Police Open Data Sets', fontsize=12)
-    
-    # Thank you Esmailian for providing a quick solution to bar annotations.
-    # Source: https://datascience.stackexchange.com/questions/48035/how-to-show-percentage-text-next-to-the-horizontal-bars-in-matplotlib
-    for p in ax.patches:
-        percentage = '{:.1f}%'.format(100 * p.get_height()/race_dis['total_reports'].sum())
-        x = p.get_x() + p.get_width() - 0.53
-        y = p.get_y() + p.get_height() + 10
-        ax.annotate(percentage, (x, y), fontsize=12)
+    # [VISUALIZATION] USE OF FORCE RACE STATS
+    for year in years:
+        race_dis = pd.DataFrame(df[df['year'] == year].race.value_counts())
+        race_dis.reset_index(inplace=True)
+        race_dis.rename(columns={'index': 'race', 'race': 'total_reports'}, inplace=True)
+        race_dis['percentage'] = ((race_dis['total_reports'] / race_dis['total_reports'].sum()) * 100)
         
-    plt.show()
+        fig, ax = plt.subplots(figsize=(10, 6))
+        plt.bar(race_dis['race'], race_dis['total_reports'], color='#E81E1E')
+        plt.title(str(year) + ' Number of Use of Force Reportsby Race', fontsize=15, y=1.03)
+        plt.xticks(rotation=30, fontsize=13)
+        plt.yticks(fontsize=13)
+        plt.ylim(0, race_dis.total_reports.max() + 20)
+        plt.text(-1, (-40 - (race_dis.total_reports.max() / 5)), 'Source: Burlington Police Open Data Sets', fontsize=12)
+    
+        # Thank you Esmailian for providing a quick solution to bar annotations.
+        # Source: https://datascience.stackexchange.com/questions/48035/how-to-show-percentage-text-next-to-the-horizontal-bars-in-matplotlib
+        for p in ax.patches:
+            percentage = '{:.1f}%'.format(100 * p.get_height()/race_dis['total_reports'].sum())
+            x = p.get_x() + p.get_width() - 0.53
+            y = p.get_y() + p.get_height() + 3
+            ax.annotate(percentage, (x, y), fontsize=12)
+            
+        plt.show()
     
     # [VISUALIZATION] POPULATION RACE STATS
     fig, ax = plt.subplots(figsize=(10, 4))
     plt.bar(c19['Race'], c19['Percentage'], color='green')    
-    plt.title('Vermont 2019 Population Percentages by Race', fontsize=15, y=1.03)
+    plt.title('Burlington 2019 Population Percentages by Race', fontsize=15, y=1.03)
     plt.xticks(rotation=90, fontsize=13)
     plt.yticks(fontsize=13)
     plt.ylim(0, 105)
-    plt.text(-1, -115, 'Source: United States Census Bureau', fontsize=12)
+    plt.text(-1, -125, 'Source: United States Census Bureau', fontsize=12)
 
     # Thank you Esmailian for providing a quick solution to bar annotations.
     # Source: https://datascience.stackexchange.com/questions/48035/how-to-show-percentage-text-next-to-the-horizontal-bars-in-matplotlib
